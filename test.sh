07ef6997df55d39afd340131f6255c1432cf79b8
lin chuan <linch1982@gmail.com>
2020-04-19 13:15:42 +0800

    update 预备课测试

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 echo "start...."
 echo "mkdir test"
 
-[ -d "./test" ] && echo "Directory ./test exists, please rename or delete it." && exit
+[ -d "./test" ] && echo "Directory ./test exists, please rename or delete it." && echo "当前目录下已经有test文件夹存在,请重命名或删除test文件夹,或者在其他目录运行该脚本" && exit
 
 mkdir test
 touch test123.txt
